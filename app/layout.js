dfed6031ff62fb626b9fa4c7c4d4dec33ab1ed28
kleefi <decharvi.charles@gmail.com>
2024-07-04 18:59:57 +0700

    fixing layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
-import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
